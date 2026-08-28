9499c0746282ffbbf93eef2052849ff257f962ad
wesnicol2 <87735869+wesnicol2@users.noreply.github.com>
2026-08-27 21:50:03 -0700

    Consolidate star UI tests

--- tests/test_starred_player_assets.py
+++
@@ -1,8 +0,0 @@
-# ruff: noqa
-from pathlib import Path
-
-
-def test_star_assets_exist():
-    ui = Path(__file__).resolve().parents[1] / "ui"
-    assert (ui / "board-stars.js").is_file()
-    assert (ui / "board-stars.css").is_file()
